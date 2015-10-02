fab75bb40c592bb6ec6006b556d98086b345330e
Christoph Witzany <christoph@web.crofting.com>
2015-10-02 19:54:21 +0200

    fix smtp_forward.ini creation

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ echo "$HQ_DOMAIN" > "$HARAKA_DIR/config/me"
 echo "$REAL_DOMAIN" > "$HARAKA_DIR/config/host_list"
 echo "dn1=cn=%u,$USER_BASE_DN" >> "$HARAKA_DIR/config/auth_ldap.ini"
 
-echo > "$HARAKA_DIR/config/smtp_forward.ini" <<EOF
+cat > "$HARAKA_DIR/config/smtp_forward.ini" <<EOF
 host=$FORWARD_HOST
 port=$FORWARD_PORT
 enable_tls=$FORWARD_USE_TLS
