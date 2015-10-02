4974342ed3ccf8cca82e65829fa4cdc50260a477
Christoph Witzany <christoph@web.crofting.com>
2015-10-02 18:23:27 +0200

    add smtp_forward.ini creation

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 HARAKA_DIR=/usr/src/app
 
-echo "$HQ_DOMAIN" > "$HARAKA_DIR/me"
-echo "$REAL_DOMAIN" > "$HARAKA_DIR/host_list"
-echo "dn1=cn=%u,$USER_BASE_DN" >> "$HARAKA_DIR/auth_ldap.ini"
+echo "$HQ_DOMAIN" > "$HARAKA_DIR/config/me"
+echo "$REAL_DOMAIN" > "$HARAKA_DIR/config/host_list"
+echo "dn1=cn=%u,$USER_BASE_DN" >> "$HARAKA_DIR/config/auth_ldap.ini"
+
+echo > "$HARAKA_DIR/config/smtp_forward.ini" <<EOF
+host=$FORWARD_HOST
+port=$FORWARD_PORT
+enable_tls=$FORWARD_USE_TLS
+auth_type=plain
+auth_user=$FORWARD_HOST_USER
+auth_pass=$FORWARD_HOST_PASSWORD
+EOF
 
 haraka -c "$HARAKA_DIR"
